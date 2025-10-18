3b9a7ed7801b45dd7b61676b3acc8b8272c79c53
Muhammad Sheraz <m.sheraz.work@gmail.com>
2025-10-18 17:01:41 +0500

    Monday.com Vercel API v13

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
 import projectRoutes from './routes/projects';
 import serverless from 'serverless-http';
-//import { createMondayWebhook } from './lib/createwebhook';
+import { createMondayWebhook } from './lib/createwebhook';
 
 dotenv.config();
 
@@ -17,14 +17,14 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 
-// app.post('/api/init-webhook', async (req, res) => {
-//   try {
-//     await createMondayWebhook();
-//     res.json({ message: 'Webhook created' });
-//   } catch (err) {
-//     console.error('Webhook failed:', err);
-//     res.status(500).send('Failed');
-//   }
-// });
+app.post('/api/init-webhook', async (req, res) => {
+  try {
+    await createMondayWebhook();
+    res.json({ message: 'Webhook created' });
+  } catch (err) {
+    console.error('Webhook failed:', err);
+    res.status(500).send('Failed');
+  }
+});
 
 export const handler = serverless(app);
